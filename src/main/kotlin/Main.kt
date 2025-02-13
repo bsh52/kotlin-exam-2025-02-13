d6ce71a64a66498b76e3362ed060b965a00ed2a3
paek1127 <paek1127@gmail.com>
2025-02-13 15:26:13 +0900

    kotlin null
    kotlin의 var val 은 기본적으로 NotNull이기 때문에 nullable 하게 변경 하려면 : String? 같이 선언 해야 한다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,7 @@
 package com.ll
 
 fun main() {
-//    val num = 20;
-//    var message = "Hello World"
-//
-//    println("num = $num")
-//    println("message = $message")
-//
-//    val number = if (true) 10 else 20
-//
-//    for (i in 0 until 3) {
-//        println("count : $i")
-//    }
-//
-//    val result = add(5, 10)
-//    println("result = $result")
-//
-//    val person1 = Person("Larry", 20)
-//    person1.greet()
+    val name: String? = null
 
-//    val names = mutableListOf("Juli", "Ali", "Jan")
-//
-//    names.add("Charlie")
-//    for (name in names) {
-//        println(name)
-//    }
-
-    val ages = mapOf("Alice" to 25, "Bob" to 30)
-
-    for ((name, age) in ages) {
-        println("$name, $age")
-    }
+    println(name?.length ?: "Name is Null")
 }
-
-//fun add(a: Int, b: Int): Int {
-//    return a + b
-//}
-//
-//class Person(val name: String, val age: Int) {
-//    fun greet() {
-//        println("Hello, $name, $age")
-//    }
-//}
