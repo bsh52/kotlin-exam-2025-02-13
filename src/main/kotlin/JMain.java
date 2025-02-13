f31afc199b6831fc2454499e95c0e23c2ddf7b9e
paek1127 <paek1127@gmail.com>
2025-02-13 14:24:48 +0900

    kotlin에서 java클래스도 사용할 수 있다

---
+++ src/main/kotlin/JMain.java
@@ -0,0 +1,8 @@
+public class JMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Person person = new Person(25, "John");
+        person.SayHello();
+    }
+}
