7449e5910d4757e8dc3f3f25357fdaa15eb2ce79
paek1127 <paek1127@gmail.com>
2025-02-13 15:44:40 +0900

    kotlin 확장 함수
    기존의 만들어진것에 추가 함수를 만들수있다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,9 @@
 package com.ll
 
-fun main() {
-    val person1 = Person("Alice", 25)
-
-    println(person1)
-
-    val person2 = Person("Alice", 25)
-
-    if (person1 == person2) {
-        println("ok")
-    }
+fun String.greet() :String {
+    return "Hello $this"
 }
 
-data class Person(val name: String, val age: Int)
+fun main() {
+    println("Jay".greet())
+}
