eb1d19f4a67cf0af2a8915a57d58aac88136adfd
paek1127 <paek1127@gmail.com>
2025-02-13 15:13:52 +0900

    kotlin list 는 불변이라 값을 넣을수 없음

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,37 @@
 package com.ll
 
 fun main() {
-    val num = 20;
-    var message = "Hello World"
+//    val num = 20;
+//    var message = "Hello World"
+//
+//    println("num = $num")
+//    println("message = $message")
+//
+//    val number = if (true) 10 else 20
+//
+//    for (i in 0 until 3) {
+//        println("count : $i")
+//    }
+//
+//    val result = add(5, 10)
+//    println("result = $result")
+//
+//    val person1 = Person("Larry", 20)
+//    person1.greet()
 
-    println("num = $num")
-    println("message = $message")
+    val names = listOf("Juli", "Ali", "Jan")
 
-    val number = if (true) 10 else 20
-
-    for (i in 0 until 3) {
-        println("count : $i")
-    }
-
-    val result = add(5, 10)
-    println("result = $result")
-
-    val person1 = Person("Larry", 20)
-    person1.greet()
-}
-
-fun add(a: Int, b: Int): Int {
-    return a + b
-}
-
-class Person(val name: String, val age: Int) {
-    fun greet() {
-        println("Hello, $name, $age")
+    for (name in names) {
+        println(name)
     }
 }
+
+//fun add(a: Int, b: Int): Int {
+//    return a + b
+//}
+//
+//class Person(val name: String, val age: Int) {
+//    fun greet() {
+//        println("Hello, $name, $age")
+//    }
+//}
